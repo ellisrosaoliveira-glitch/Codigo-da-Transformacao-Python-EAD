3a374410cc66ac9ae3f04a344b23af0ea98884ed
aluno em vocacao <ellisrosaoliveira@gmail.com>
2026-07-08 14:53:07 -0300

    upgrade_progresso

---
+++ modulo02/modulo02_intro_python_ellis-ativ02.py
@@ -0,0 +1,14 @@
+
+print("Olá, este é um teste do interpretador Python!")
+
+texto = "Estudando Python"
+numero_inteiro = 2026
+numero_decimal = 7.5
+valor_booleano = True
+
+
+print("\nVerificando os tipos de dados:")
+print("O tipo da variável 'texto' é:", type(texto))
+print("O tipo da variável 'numero_inteiro' é:", type(numero_inteiro))
+print("O tipo da variável 'numero_decimal' é:", type(numero_decimal))
+print("O tipo da variável 'valor_booleano' é:", type(valor_booleano))
